17207b72a65607f1f376e061bad31bac92ee5e2b
Ishaan Sharma <ishaansharma1975@gmail.com>
2023-11-29 20:14:31 +0530

    graph fixed

--- MyApp.py
+++ MyApp.py
@@ -8,8 +8,7 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
-
-st.title("Stock Price Prediction App")
+st.title("Stock Price Analysis App")
 
 project_folder = Path(os.getcwd() + "/dataset")
 
@@ -21,6 +20,7 @@ if selected_dataset is not None:
     data = pd.read_csv(selected_dataset)
 
     features = [
+        "Date",
         "Open",
         "High",
         "Low",
@@ -30,48 +30,41 @@ if selected_dataset is not None:
         "Volume",
         "Turnover",
         "Trades",
-        # "Deliverable Volume",
-        # "%Deliverble",
+        "Deliverable Volume",
+        "%Deliverble"
     ]
     target = "Close"
 
     data = data.dropna()
+    data["Date"] = pd.to_datetime(data["Date"])
 
     X = data[features]
     y = data[target]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
+    X_train.iloc[:, 1:] = scaler.fit_transform(X_train.iloc[:, 1:])
+    X_test.iloc[:, 1:] = scaler.transform(X_test.iloc[:, 1:])
 
     model = XGBRegressor(objective="reg:squarederror")
-    model.fit(X_train, y_train)
+    model.fit(X_train.iloc[:, 1:], y_train)
 
-    y_pred = model.predict(X_test)
+    y_pred = model.predict(X_test.iloc[:, 1:])
 
     mse = mean_squared_error(y_test, y_pred)
-
     st.subheader(f"Mean Squared Error: {mse}")
 
+    X_test_sorted = X_test.sort_values(by="Date")
+
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(
-        data["Date"].iloc[-len(y_test) :],
-        y_test.values,
-        label="Actual Closing Rate",
-        marker="o",
-    )
-    ax.plot(
-        data["Date"].iloc[-len(y_test) :],
-        y_pred,
-        label="Predicted Closing Rate",
-        marker="o",
-    )
+    ax.plot(X_test["Date"], y_test.values, label="Actual Closing Rate", marker="o")
+    ax.plot(X_test["Date"], y_pred, label="Predicted Closing Rate", marker="o")
     ax.legend()
     ax.set_title(Path(selected_dataset).stem)
     ax.set_xlabel("Date")
     ax.set_ylabel("Closing Rate")
     ax.tick_params(rotation=45)
+    ax.set_xticklabels([])
+    ax.set_xticks([])
     st.pyplot(fig)
